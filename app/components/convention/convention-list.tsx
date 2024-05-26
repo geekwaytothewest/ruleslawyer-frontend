ef5c479faa0de9d0cda2d2e0ede60049c31d1e6a
Mattie Schraeder <mattie@mattie.lgbt>
2024-05-26 04:14:37 -0500

    expand the active convention

--- app/components/convention/convention-list.tsx
+++ app/components/convention/convention-list.tsx
@@ -1,18 +1,22 @@
 "use client";
 import { Accordion, AccordionItem } from "@nextui-org/react";
-import React from "react";
+import React, { act } from "react";
 import ConventionInfo from "./convention-info";
 
 export default function ConventionList(props: any) {
   let { conventions } = props;
 
+  let activeConvention = conventions.find(
+    (c: any) => c.startDate < Date.now() && Date.parse(c.endDate) > Date.now()
+  );
+
+  if (activeConvention === undefined) {
+    activeConvention = conventions.find((c: any) => Date.parse(c.startDate) > Date.now());
+  }
   return (
-    <Accordion variant="bordered">
+    <Accordion variant="bordered" defaultExpandedKeys={[String(activeConvention?.id)]}>
       {conventions.map(
-        (c: {
-          id: React.Key | null | undefined;
-          name: string
-        }) => {
+        (c: { id: React.Key | null | undefined; name: string }) => {
           return (
             <AccordionItem key={c.id} aria-label={c.name} title={c.name}>
               <ConventionInfo id={c.id} />
