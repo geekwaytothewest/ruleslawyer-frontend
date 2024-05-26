ee3caa5b65531e0ae345857ebdb477a56cfaecb8
Mattie Schraeder <mattie@mattie.lgbt>
2024-05-26 10:35:27 -0500

    cleanup imports

--- app/components/convention/convention-list.tsx
+++ app/components/convention/convention-list.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Accordion, AccordionItem } from "@nextui-org/react";
-import React, { act } from "react";
+import React from "react";
 import ConventionInfo from "./convention-info";
 
 export default function ConventionList(props: any) {
