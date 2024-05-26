f90af5653392cfe452a3f1fa1a0bfb07b87f9eee
Mattie Schraeder <mattie@mattie.lgbt>
2024-05-26 10:18:18 -0500

    cleanup imports

--- app/dashboard/organization/[orgId]/convention/[conId]/page.tsx
+++ app/dashboard/organization/[orgId]/convention/[conId]/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import ConventionInfo from "@/app/components/convention/convention-info";
-import frontendFetch from "@/utilities/frontendFetch";
-import { useSession } from "next-auth/react";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 export default function ConView({ params }: { params: { orgId: string, conId: string } }) {
   return (
