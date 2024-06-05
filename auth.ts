7e6f9da10c96ce7e8c0ba44cdce7bcb0ad24c663
Mattie Schraeder <mattie@mattie.lgbt>
2024-06-05 12:12:44 -0500

    refresh tokens maybe work?

--- auth.ts
+++ auth.ts
@@ -7,7 +7,12 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       clientId: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
       issuer: process.env.AUTH0_ISSUER_BASE_URL,
-      authorization: { params: { scope: "openid email profile", audience: process.env.AUTH0_AUDIENCE } },
+      authorization: {
+        params: {
+          scope: "openid email profile offline_access",
+          audience: process.env.AUTH0_AUDIENCE,
+        },
+      },
     }),
   ],
   session: {
@@ -20,10 +25,44 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         // set access_token to the token payload
         token.accessToken = account.access_token;
         token.expiresAt = account.expires_at;
+        token.refreshToken = account.refresh_token;
       }
 
-      if (new Date().getMilliseconds() < (token.expiresAt as number) * 1000) {
-        signOut();
+      if (
+        token.expiresAt &&
+        new Date().getMilliseconds() < (token.expiresAt as number) * 1000 &&
+        token.refreshToken
+      ) {
+        try {
+          // The `token_endpoint` can be found in the provider's documentation. Or if they support OIDC,
+          // at their `/.well-known/openid-configuration` endpoint.
+          // i.e. https://accounts.google.com/.well-known/openid-configuration
+          const response = await fetch("https://oauth2.googleapis.com/token", {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams({
+              client_id: process.env.AUTH0_CLIENT_ID!,
+              client_secret: process.env.AUTH0_CLIENT_SECRET!,
+              grant_type: "refresh_token",
+              refresh_token: token.refresh_token as string,
+            }),
+            method: "POST",
+          })
+
+          const responseTokens = await response.json()
+
+          if (!response.ok) throw responseTokens
+
+          return {
+            ...token,
+            accessToken: responseTokens.access_token,
+            expiresAt: Math.floor(Date.now() / 1000 + (responseTokens.expires_in as number)),
+            refreshToken: responseTokens.refresh_token ?? token.refresh_token,
+          }
+        } catch (error) {
+          console.error("Error refreshing access token", error)
+          // The error property can be used client-side to handle the refresh token error
+          return { ...token, error: "RefreshAccessTokenError" as const }
+        }
       }
 
       return token;
