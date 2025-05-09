49987a9362e52ca18d377b591744328b73538ca7
Mattie Schraeder <mattie@mattie.lgbt>
2025-05-08 19:25:31 -0500

    attempt to fix nextjs-auth0 cve

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,11 @@
 import {
-  withMiddlewareAuthRequired,
-  getSession,
-} from "@auth0/nextjs-auth0/edge";
+  withApiAuthRequired,
+} from "@auth0/nextjs-auth0";
 import { NextResponse } from "next/server";
 
 export default withMiddlewareAuthRequired(async function middleware(req) {
   const res = NextResponse.next();
-  const user = await getSession(req, res);
+  const user = await auth0.getSession(req, res);
   return res;
 });
 
