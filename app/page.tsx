3bd71e5722bec3abc750356864a920c75951a7ab
Mattie Schraeder <mattie@mattie.lgbt>
2026-05-23 02:46:32 -0500

    fix lgo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import geekgayLogo from "@/public/geekgay.png";
 import Profile from "@/components/profile";
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Image
-        src="/geekgay.png"
+        src={geekgayLogo}
         width={500}
         height={436}
         alt="Geekway to the West Pride Logo"
