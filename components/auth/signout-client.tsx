c26bf9bc7d8825d485f7fa232bdc0355a343b1e5
Mattie Schraeder <mattie@mattie.lgbt>
2026-05-27 02:09:15 -0500

    collapsable and fix the conventions buttons

--- components/auth/signout-client.tsx
+++ components/auth/signout-client.tsx
@@ -3,11 +3,11 @@
 import React from "react";
 import { VscSignOut } from "react-icons/vsc";
 
-export function SignOut() {
+export function SignOut({ collapsed = false }: { collapsed?: boolean }) {
   return (
-    <button onClick={() => { window.location.href = `${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/auth/logout`; }} className="hover:text-gwgreen hover:cursor-pointer">
+    <button onClick={() => { window.location.href = `${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/auth/logout`; }} className="hover:text-gwgreen hover:cursor-pointer" aria-label="Sign Out" title="Sign Out">
       <span className="text-lg">
-        <VscSignOut className="inline-block" /> Sign Out
+        <VscSignOut className="inline-block" />{!collapsed && " Sign Out"}
       </span>
     </button>
   );
